9cb59a043d015dd8c36c6e1cb99373282417519e
Abhishek Sharma <sharma312006@gmail.com>
2024-04-17 23:51:24 +0530

    create signin controller

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
@@ -27,5 +28,32 @@ export const signup = async (req, res, next) => {
     });
   } catch (error) {
     next(errorHandler(500, error.message));
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || username == "" || password == "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email }).select("-password");
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    console.log(validUser);
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+    const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(validUser);
+  } catch (error) {
+    return next(errorHandler(500, error.message));
   }
 };
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { signup } from "../controllers/auth.controllers.js";
+import { signIn, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
-
+router.post("/sign-in", signIn);
 export default router;
