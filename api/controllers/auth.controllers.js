531d5a109d99bb9279518af54200b254a4839a20
Abhishek Sharma <sharma312006@gmail.com>
2024-04-17 13:46:12 +0530

    comple signup page ui

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
   if (
@@ -11,7 +12,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({
@@ -25,6 +26,6 @@ export const signup = async (req, res) => {
       message: "SignUp Successful",
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(errorHandler(500, "Internal server error"));
   }
 };
