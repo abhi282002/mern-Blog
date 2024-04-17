ad51de2e7e1645bdd47c444656a714ff79807ad5
Abhishek Sharma <sharma312006@gmail.com>
2024-04-17 10:20:31 +0530

    add all required Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 export default function App() {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
